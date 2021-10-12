e845e228ca27ce8abd295eafbddee2376c0a7326
yuwinzer <yu.moskalev@gmail.com>
2021-10-12 10:37:34 +0300

    updated 1 task and 2 task is done

--- lesson_4/task2_les4.py
+++ lesson_4/task2_les4.py
@@ -1,15 +1,24 @@
 from pprint import pprint
 from lxml import html
 import requests
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError as dke
+
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
 
-response = requests.get("https://lenta.ru/")
-dom = html.fromstring(response.text)
+# create mongo database
+client = MongoClient('127.0.0.1', 27017)
+db = client['news']
+news_collection = db.news_collection
 
-items = dom.xpath("//time[@class='g-time']/../..")
+news_collection.delete_many({})  # get empty before start
 
 all_news = []
+
+response = requests.get("https://lenta.ru/")
+dom = html.fromstring(response.text)
+items = dom.xpath("//time[@class='g-time']/../..")
 
 for item in items:
     new = {}
@@ -21,16 +30,23 @@ for item in items:
     link = item.xpath(".//time[@class='g-time']/../../a/@href")
     link = ''.join(link)
 
-    if (link[1:5]) == 'news':  # sometime here can be not a lenta.ru sourse, but moslenta.ru
+    if (link[1:5]) == 'news':  # sometime here can be not a lenta.ru sourse, but external link to moslenta.ru
         source = 'lenta.ru'
+        new['_id'] = link.split('/')[5]
     else:
         source = (link.split('/'))[2]
-
+        # new['_id'] = need to catch this moslenta.ru case and get ID from link
 
     new['name'] = name
     new['link'] = link
     new['datetime'] = datetime
     new['source'] = source
+
+    try:
+        news_collection.insert_one(new)
+    except dke:
+        pass
+
     all_news.append(new)
 
 pprint(all_news)
